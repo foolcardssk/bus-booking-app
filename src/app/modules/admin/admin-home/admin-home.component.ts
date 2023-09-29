7e60f3dc1da3159e6378a3610047cf9c7bb81055
foolcardssk <foolcardssk@gmail.com>
2023-09-29 21:30:00 +0530

    [Worked] Admin home page
    Aparrently different browsers handle svg differently

--- src/app/modules/admin/admin-home/admin-home.component.ts
+++ src/app/modules/admin/admin-home/admin-home.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-admin-home',
-  templateUrl: './admin-home.component.html',
-  styleUrls: ['./admin-home.component.css']
+    selector: 'app-admin-home',
+    templateUrl: './admin-home.component.html',
+    styleUrls: ['./admin-home.component.css']
 })
 export class AdminHomeComponent {
 
+    constructor(private router: Router) { }
+
+    onManage() {
+        this.router.navigate(['/admin/manage']);
+    }
+    onBookingLogs() {
+        this.router.navigate(['/admin/booking-logs']);
+    }
+    onUserView() {
+        this.router.navigate(['/traveller/home']);
+    }
 }
